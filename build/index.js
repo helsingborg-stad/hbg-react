f1adf7ec8fd55e225f155f6c260bd1b96b0135a9
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-10-05 15:46:32 +0200

    Rebuild

--- build/index.js
+++ build/index.js
@@ -401,6 +401,15 @@ Object.defineProperty(exports, 'Dropdown', {
   }
 });
 
+var _WordpressAuthForm = __webpack_require__(21);
+
+Object.defineProperty(exports, 'WordpressAuthForm', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_WordpressAuthForm).default;
+  }
+});
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /***/ }),
@@ -20612,6 +20621,220 @@ exports.unstable_unsubscribe = unstable_unsubscribe;
 }
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+
+/***/ }),
+/* 21 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Title = function Title(props) {
+    if (!props.children) {
+        return null;
+    }
+
+    return React.createElement(
+        'h3',
+        null,
+        props.children
+    );
+};
+
+var Content = function Content(props) {
+    if (!props.children) {
+        return null;
+    }
+
+    return React.createElement(
+        'p',
+        null,
+        props.children
+    );
+};
+
+var Notice = function Notice(props) {
+    var message = props.message,
+        type = props.type;
+
+
+    if (!message) {
+        return null;
+    }
+
+    var classes = 'notice';
+    var avalibleTypes = ['error', 'success', 'warning'];
+
+    if (avalibleTypes.includes(type)) {
+        classes += ' notice-' + type;
+    }
+
+    return React.createElement(
+        'div',
+        { className: classes },
+        React.createElement(
+            'p',
+            null,
+            message
+        )
+    );
+};
+
+var Field = function Field(props) {
+    var id = props.id,
+        label = props.label,
+        value = props.value,
+        readOnly = props.readOnly,
+        onChangeAction = props.onChangeAction;
+
+    var Field = function Field() {
+        return React.createElement('input', { type: 'text', name: id, id: id, onChange: onChangeAction, defaultValue: value });
+    };
+
+    //Read only field
+    if (readOnly && value && value != '') {
+        Field = function Field() {
+            return React.createElement(
+                'code',
+                null,
+                value
+            );
+        };
+    }
+
+    return React.createElement(
+        'tr',
+        null,
+        React.createElement(
+            'th',
+            null,
+            React.createElement(
+                'label',
+                { htmlFor: !readOnly ? id : null },
+                label
+            )
+        ),
+        React.createElement(
+            'td',
+            null,
+            React.createElement(Field, null)
+        )
+    );
+};
+
+var SubmitButton = function SubmitButton(props) {
+    var text = props.text,
+        style = props.style;
+
+    var avalibleStyles = ['primary'];
+
+    var classes = 'button';
+
+    if (avalibleStyles.includes(style)) {
+        classes = 'button-' + style;
+    }
+
+    return React.createElement('input', { name: 'submit', type: 'submit', className: classes, value: text });
+};
+
+module.exports = function (_React$Component) {
+    _inherits(_class, _React$Component);
+
+    function _class(props) {
+        _classCallCheck(this, _class);
+
+        var _this = _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).call(this, props));
+
+        _this.state = {
+            fields: props.fields
+        };
+        return _this;
+    }
+
+    _createClass(_class, [{
+        key: 'submitForm',
+        value: function submitForm(e) {
+            e.preventDefault();
+            var onSubmitAction = this.props.onSubmitAction;
+
+            if (onSubmitAction) {
+                onSubmitAction(this.state.fields);
+            }
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var _this2 = this;
+
+            var _props = this.props,
+                title = _props.title,
+                content = _props.content,
+                notice = _props.notice,
+                noticeType = _props.noticeType,
+                submitButtonText = _props.submitButtonText,
+                submitButtonStyle = _props.submitButtonStyle;
+            var fields = this.state.fields;
+
+
+            return React.createElement(
+                'form',
+                { onSubmit: this.submitForm.bind(this) },
+                React.createElement(Notice, {
+                    message: notice || null,
+                    type: noticeType || null
+                }),
+                React.createElement(
+                    Title,
+                    null,
+                    title
+                ),
+                React.createElement(
+                    Content,
+                    null,
+                    content
+                ),
+                React.createElement(
+                    'table',
+                    { className: 'form-table' },
+                    React.createElement(
+                        'tbody',
+                        null,
+                        fields.map(function (field, index) {
+                            return React.createElement(Field, {
+                                key: field.id || 'field-' + index,
+                                id: field.id,
+                                label: field.label,
+                                value: field.value,
+                                readOnly: field.readOnly,
+                                onChangeAction: function onChangeAction(e) {
+                                    var value = e.target.value;
+                                    _this2.setState(function (prevState) {
+                                        var fields = prevState.fields;
+                                        fields[index].value = value;
+                                    });
+                                }
+                            });
+                        })
+                    )
+                ),
+                React.createElement(SubmitButton, {
+                    text: submitButtonText || 'Submit',
+                    style: submitButtonStyle
+                })
+            );
+        }
+    }]);
+
+    return _class;
+}(React.Component);
 
 /***/ })
 /******/ ]);
