30a3f12418f75380fc066c800de337f6c92a1f7c
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-10-05 15:37:20 +0200

    Remove inherited list style

--- src/Dropdown/DropdownList.js
+++ src/Dropdown/DropdownList.js
@@ -1,8 +1,8 @@
 module.exports = (props) => {
     return (
         <div className="c-dropdown__menu is-open">
-            <ul className="o-dropdown-links">
-                {props.children.map((child) => (<li>{child}</li>))}
+            <ul className="o-dropdown-links unlist">
+                {props.children.map((child, index) => (<li key={index}>{child}</li>))}
             </ul>
         </div>
     );
