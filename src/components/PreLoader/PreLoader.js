7be5636e25b2952da242ec8c7f58b6a738ff7c57
nRamstedt <nikolas.ramstedt@gmail.com>
2021-01-13 13:32:46 +0100

    Implement new loader markup

--- src/components/PreLoader/PreLoader.js
+++ src/components/PreLoader/PreLoader.js
@@ -2,12 +2,7 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 const PreLoader = ({ modifier = "" }) => (
-    <div className={`loading ${modifier}`}>
-        <div />
-        <div />
-        <div />
-        <div />
-    </div>
+    <div className={`c-loader c-loader__linear--color--black c-loader__linear c-loader__linear--md ${modifier}`}></div>
 );
 
 export default PreLoader;
