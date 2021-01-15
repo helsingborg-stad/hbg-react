2017156fa1033d6c031f437b1e50678d6ce4d703
nRamstedt <nikolas.ramstedt@gmail.com>
2021-01-15 11:48:32 +0100

    decrease preloader size

--- src/components/PreLoader/PreLoader.js
+++ src/components/PreLoader/PreLoader.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 const PreLoader = ({ modifier = "" }) => (
-    <div className={`c-loader c-loader__linear--color--black c-loader__linear c-loader__linear--md ${modifier}`}></div>
+    <div className={`c-loader c-loader__linear--color--black c-loader__linear c-loader__linear c-loader__linear--sm ${modifier}`}></div>
 );
 
 export default PreLoader;
